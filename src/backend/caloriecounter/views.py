8208ae12bac56c7c166eb8b2f86b86054dbd4991
Shreyash Raj <shreyashr267@gmail.com>
2023-01-19 19:09:43 +0530

    completed api implement

--- src/backend/caloriecounter/views.py
+++ src/backend/caloriecounter/views.py
@@ -2,6 +2,9 @@ from rest_framework import viewsets
 from .serializers import UserFoodItemSerializer
 from rest_framework import permissions
 from .models import UserFoodItem
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils import timezone
 
 # Create your views here.
 class UserFoodItemViewSet(viewsets.ModelViewSet):
@@ -10,6 +13,12 @@ class UserFoodItemViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return UserFoodItem.objects.filter(user=self.request.user)
-        
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+    
+    @action(detail=False, methods=['get'])
+    def today(self, request):
+        queryset = UserFoodItem.objects.filter(user=request.user, date=timezone.now())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
