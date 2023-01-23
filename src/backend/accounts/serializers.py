e2c5408737071213b9015beda0a947a1c7db9933
Shreyash Raj <shreyashr267@gmail.com>
2023-01-23 17:29:19 +0530

    solved patch bug in user model

--- src/backend/accounts/serializers.py
+++ src/backend/accounts/serializers.py
@@ -4,11 +4,10 @@ from .models import CustomUser as User
 from rest_framework.authtoken.models import Token
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    max_calories = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = ('url', 'id', 'username', 'email', 'password', 'max_calories')
-        extra_kwargs = {'password': {'write_only': True}, 'email': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True}}
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
@@ -16,7 +15,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     
     def update(self, instance, validated_data):
         super().update(instance, validated_data)
-        password = validated_data.get('password', None)
+        password = validated_data.get('password')
         if password:
             user = User.objects.get(username=instance.username)
             user.set_password(password)
@@ -25,6 +24,13 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     
     def get_max_calories(self, obj):
         return obj.max_calories if self.context['request'].user.id == obj.id else None
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if self.context['request'].user != instance:
+            representation['email'] = None
+            representation['max_calories'] = None
+        return representation
 
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
