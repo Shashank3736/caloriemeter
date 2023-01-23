673d581029eec4b6a3444b51deb4ee4b07620b80
Shreyash Raj <shreyashr267@gmail.com>
2023-01-24 04:06:17 +0530

    Support profile menu feature

--- src/old-frontend/src/components/Navbar.tsx
+++ src/old-frontend/src/components/Navbar.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from '@mui/material/Button'
 import Tooltip from '@mui/material/Tooltip'
 import IconButton from '@mui/material/IconButton'
-import { DarkMode, Home, LightMode, Login, SettingsBrightness } from '@mui/icons-material'
+import { Home, Login, Logout, Settings, Today } from '@mui/icons-material'
+import { get_user } from '@/utils/api'
+import { Avatar, Divider, FormControlLabel, ListItemIcon, Menu, MenuItem, Switch } from '@mui/material'
 
 const name = 'Shreyash Raj'
 const links = [
@@ -15,6 +18,30 @@ const links = [
 ]
 
 const Navbar = ({darkMode, toggleDarkMode}: {darkMode: 'light' | 'dark', toggleDarkMode: () => void}) => {
+    const [user, setUser] = useState(null)
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const logout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('id')
+        localStorage.removeItem('mode')
+        setUser(null)
+    };
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        const id = localStorage.getItem('id')
+        if (!token || !id) return
+        get_user({ token, id }).then((user) => {
+            setUser(user)
+        })
+    }, [])
     return (
         <header>
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
@@ -28,12 +55,87 @@ const Navbar = ({darkMode, toggleDarkMode}: {darkMode: 'light' | 'dark', toggleD
                             <Button color='inherit' href={link.href} key={link.name} startIcon={link.icon}>{link.name}</Button>
                         )
                     })}
-                    <Button className='ml-5' variant='contained' href='/accounts/login' startIcon={<Login />}>Login</Button>
-                    <Tooltip title={darkMode.toUpperCase()}>
-                        <IconButton onClick={toggleDarkMode} className='ml-5'>
-                            {darkMode === 'light' ? <DarkMode /> : <LightMode />}
+                    {(user === null) ? <Button className='ml-5' variant='contained' href='/accounts/login' startIcon={<Login />}>Login</Button>:
+                    <>
+                    <Tooltip title='Profile'>
+                        <IconButton 
+                        onClick={handleClick}
+                        size='small'
+                        className='ml-5'
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}>
+                            <Avatar sx={{ width: 32, height: 32}}>{user.username[0].toUpperCase()}</Avatar>
                         </IconButton>
                     </Tooltip>
+                    <Menu
+                        anchorEl={anchorEl}
+                        id="account-menu"
+                        open={open}
+                        onClose={handleClose}
+                        onClick={handleClose}
+                        PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                            width: 32,
+                            height: 32,
+                            ml: -0.5,
+                            mr: 1,
+                            },
+                            '&:before': {
+                            content: '""',
+                            display: 'block',
+                            position: 'absolute',
+                            top: 0,
+                            right: 14,
+                            width: 10,
+                            height: 10,
+                            bgcolor: 'background.paper',
+                            transform: 'translateY(-50%) rotate(45deg)',
+                            zIndex: 0,
+                            },
+                        },
+                        }}
+                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        
+                        <MenuItem onClick={(e) => {
+                            e.stopPropagation();
+                            toggleDarkMode();
+                        }}>
+                            <FormControlLabel
+                            value='darkMode'
+                            control={<Switch checked={darkMode === 'dark'} onChange={toggleDarkMode} />}
+                            label='Dark Mode'
+                            labelPlacement='start'
+                            />
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem onClick={() => window.location.replace('/me/today/')}>
+                            <ListItemIcon>
+                                <Today fontSize="small" />
+                            </ListItemIcon>
+                            Today
+                        </MenuItem>
+                        <MenuItem onClick={() => window.location.replace('/me/settings/')}>
+                            <ListItemIcon>
+                                <Settings fontSize="small" />
+                            </ListItemIcon>
+                            Settings
+                        </MenuItem>
+                        <MenuItem onClick={logout}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                            Logout
+                        </MenuItem>
+                    </Menu>
+                    </>}
                 </nav>
             </div>
         </header>
