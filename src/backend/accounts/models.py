4be45c0c13865a2edff7701daf6fa36d5b3414f3
Shreyash Raj <shreyashr267@gmail.com>
2023-01-23 16:58:17 +0530

    change the id into random 18 digit integer

--- src/backend/accounts/models.py
+++ src/backend/accounts/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import random
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     email = models.EmailField(unique=True)
     max_calories = models.FloatField(default=2_000)
 
     def __str__(self):
         return self.username
+    
+    # create random 18 digit integer as user id
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = random.randint(10**17, 10**18-1)
+        super().save(*args, **kwargs)
 
