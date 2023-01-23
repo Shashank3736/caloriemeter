24bcc92e0f2fc62733712a8eb50b8a3fe468aec6
Shreyash Raj <shreyashr267@gmail.com>
2023-01-24 04:05:46 +0530

    v1.2.0 - Food id is 18 digit number

--- src/backend/caloriecounter/models.py
+++ src/backend/caloriecounter/models.py
@@ -1,5 +1,6 @@
 from accounts.models import CustomUser as User
 from django.db import models
+import random
 
 # Create your models here.
 class UserFoodItem(models.Model):
@@ -9,8 +10,15 @@ class UserFoodItem(models.Model):
         ('dinner','Dinner'),
         ('snacks','Snacks'),
     )
+
+    id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=50, choices=options)
     name = models.CharField(max_length=200)
     calorie = models.FloatField()
     date = models.DateField()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = random.randint(10**17, 10**18-1)
+        super().save(*args, **kwargs)
