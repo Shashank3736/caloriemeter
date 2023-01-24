ae302144a50aab0d4236ba001966aa533aeb0109
Shreyash Raj <shreyashr267@gmail.com>
2023-01-24 16:20:45 +0530

    improved padding and tagline

--- src/old-frontend/src/components/Footer.tsx
+++ src/old-frontend/src/components/Footer.tsx
@@ -10,12 +10,12 @@ const name = 'Shreyash Raj'
 const Footer = () => {
   return (
     <footer className=''>
-        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <div className="container px-5 mx-auto flex items-center sm:flex-row flex-col">
             <Link className='flex font-medium items-center md:justify-start justify-center no-underline text-gray-900 dark:text-white' href="/">
                 <Image src="/favicon.ico" alt="Logo" width={32} height={32} />
                 <span className="ml-3 text-xl">{name}</span>
             </Link>
-            <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:border-solid border-0 sm:border-l-2 sm:border-gray-200 mt-4">© {new Date().getFullYear()} Tailblocks —
+            <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:border-solid border-0 sm:border-l-2 sm:border-gray-200 mt-4">© {new Date().getFullYear()} Caloriemeter —
                 <a href="https://twitter.com/Shashank3736" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@Shashank3736</a>
             </p>
             <div className='inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start'>
