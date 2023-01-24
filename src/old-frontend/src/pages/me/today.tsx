a0e3a543174f7afeacf36efc11f1d2090c873c00
Shreyash Raj <shreyashr267@gmail.com>
2023-01-24 18:25:24 +0530

    improvements in UI and api support

--- src/old-frontend/src/pages/me/today.tsx
+++ src/old-frontend/src/pages/me/today.tsx
@@ -1,9 +1,11 @@
 import MainLayout from '@/layout/MainLayout'
-import { Food, create_food, get_today_foods } from '@/utils/api'
-import { AddCircle } from '@mui/icons-material'
-import { Container, Divider, FormControl, FormGroup, IconButton, InputLabel, MenuItem, Paper, Select, SelectChangeEvent, TextField, Typography } from '@mui/material'
+import { Food, create_food, delete_food, get_today_foods, get_user } from '@/utils/api'
+import { AddCircle, Close, Delete } from '@mui/icons-material'
+import { Alert, Button, Container, Divider, FormControl, FormGroup, IconButton, InputLabel, MenuItem, Paper, Select, SelectChangeEvent, Snackbar, TextField, Tooltip, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css';
 
 type Props = {
     darkMode: 'light' | 'dark'
@@ -15,6 +17,8 @@ const Today = ({ darkMode, toggleDarkMode }: Props) => {
         foodType: '',
         food: '',
         foods: [],
+        max_calories: 2000,
+        calories: 0,
     });
 
     const CATEG = ['breakfast', 'lunch', 'dinner', 'snacks']
@@ -34,31 +38,78 @@ const Today = ({ darkMode, toggleDarkMode }: Props) => {
                 ...state,
                 foods: [...state.foods, res],
                 food: '',
-                foodType: ''
+                foodType: '',
+                calories: state.calories + res.calorie
+            })
+        }).catch((err) => {
+            console.log(err)
+            setState({
+                ...state,
+                food: '',
+                foodType: '',
+            })
+        })
+    }
+
+    function deleteFood(id: string, calorie: number) {
+        const token = localStorage.getItem('token')
+        if (!token) return window.location.replace('/accounts/login')
+        delete_food({ token, id }).then((res) => {
+            const foods = state.foods.filter((food: Food) => food.id !== id)
+            setState({
+                ...state,
+                foods,
+                calories: state.calories - calorie
             })
         })
     }
 
     React.useEffect(() => {
         const token = localStorage.getItem('token')
-        if (!token) return window.location.replace('/accounts/login')
+        const id = localStorage.getItem('id')
+        if (!token || !id) return window.location.replace('/accounts/login')
         get_today_foods({ token }).then((res) => {
+            let calories = 0
+            res.forEach((food: Food) => {
+                calories += food.calorie
+            })
             setState({
                 ...state,
-                foods: res
+                foods: res,
+                calories
             })
-            console.log(res)
         }).catch((err) => {
             console.log(err)
         })
+
+        get_user({ token, id }).then((res) => {
+            setState({
+                ...state,
+                max_calories: res.max_calories
+            })
+        }).catch((err) => {
+            console.log(err)
+        })
+        console.log(state)
     },[])
   return (
     <MainLayout darkMode={darkMode} toggleDarkMode={toggleDarkMode}>
-        <Container className='w-[100vw]'>
+        <Container className='w-[100vw] relative'>
+            <Typography variant='h4' className='text-center font-bold'>Today</Typography>
+            <Box className='flex justify-center items-center overflow-auto mt-3'>
+                <Typography variant='h6' className='font-bold mr-4'>Total Calories: <span className={state.max_calories >= state.calories ? 'text-blue-500': 'text-red-500'}>{state.calories.toFixed(2)}</span>/<strong>{state.max_calories}</strong></Typography>
+                <CircularProgressbarWithChildren className='h-32 w-32' styles={buildStyles({
+                    pathColor: state.max_calories >= state.calories ? '#3b82f6' : '#ef4444',
+                    textColor: state.max_calories >= state.calories ? '#3b82f6' : '#ef4444',
+                })} value={state.calories*100/state.max_calories}>
+                    {state.max_calories >= state.calories ? <Typography variant='h6' className='font-bold'>{(state.calories*100/state.max_calories).toFixed(2)}%</Typography> : <Typography variant='h6' className='font-bold'>Over</Typography>}
+                </CircularProgressbarWithChildren>
+            </Box>
+            <Divider className='my-5' />
             <FormGroup className='flex md:flex-row mx-auto items-center justify-center'>
                 <FormControl className='w-[15vw] min-w-[150px]'>
                     <InputLabel id='food-type'>Type</InputLabel>
-                    <Select id='select-food-type' labelId='food-type' label='Type' value={state.foodType} className='rounded-none' onChange={handleTypeChange}>
+                    <Select id='select-food-type' labelId='food-type' label='Type' value={state.foodType} onChange={handleTypeChange}>
                         <MenuItem value='breakfast'>Breakfast</MenuItem>
                         <MenuItem value='lunch'>Lunch</MenuItem>
                         <MenuItem value='dinner'>Dinner</MenuItem>
@@ -67,7 +118,12 @@ const Today = ({ darkMode, toggleDarkMode }: Props) => {
                 </FormControl>
                 <TextField label='Food' value={state.food} variant='outlined' className='w-[30vw] min-w-[200px]' 
                 InputProps={{
-                    endAdornment: <IconButton onClick={addFood}><AddCircle color='inherit' /></IconButton>
+                    endAdornment:
+                    <Tooltip title='Add Food'> 
+                        <IconButton onClick={addFood}>
+                            <AddCircle color='inherit' />
+                        </IconButton>
+                    </Tooltip>
                 }}
                 onChange={(e) => setState({ ...state, food: e.target.value })}
                 />
@@ -75,18 +131,23 @@ const Today = ({ darkMode, toggleDarkMode }: Props) => {
             <Box className='flex flex-wrap py-10'>
                 {CATEG.map((categ) => {
                     return (
-                        <Box key={categ} className='p-4 md:w-full lg:w-1/2'>
-                            <Paper className='h-full'>
-                                <Typography variant='h5' className='text-center p-8'>{categ.toUpperCase()}</Typography>
+                        <Box key={categ} className='p-4 w-full md:w-1/2'>
+                            <Paper className='h-full p-4 m-4 rounded'>
+                                <Typography variant='body2' className='text-center p-4 font-semibold'>{categ.toUpperCase()}</Typography>
                                 <Divider />
                                 {state.foods.filter((food: Food) => food.category === categ).map((food: Food) => {
                                     return (
-                                        <Box key={food.id} className='flex items-center justify-between px-8 py-5'>
+                                        <>
+                                        <Box key={food.id} className='flex items-center justify-between px-8 py-1'>
                                             <Typography>{food.name}</Typography>
                                             <Typography>{food.calorie} kcal</Typography>
+                                            <IconButton onClick={() => deleteFood(food.id, food.calorie)} color='error'><Delete /></IconButton>
                                         </Box>
+                                        <Divider />
+                                        </>
                                     )
                                 })}
+                                {state.foods.filter((food: Food) => food.category === categ).length === 0 && <Typography variant='caption' className='flex justify-center py-3 text-gray-400'>No food added</Typography>}
                             </Paper>
                         </Box>
                     )
