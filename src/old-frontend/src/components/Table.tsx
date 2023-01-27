9d5e5505dbb6c1c084f78bcecf2fb7f734239aab
Shreyash Raj <shreyashr267@gmail.com>
2023-01-27 22:36:49 +0530

    custom table

---
+++ src/old-frontend/src/components/Table.tsx
@@ -0,0 +1,21 @@
+import { TableCell, TableRow, styled, tableCellClasses } from "@mui/material";
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    },
+}));
+  
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+    border: 0,
+    },
+}));
