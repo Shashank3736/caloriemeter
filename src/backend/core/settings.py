ec75bfe333f57b1ae050218b356d909df11dfad5
Shreyash Raj <shreyashr267@gmail.com>
2023-01-28 11:39:37 +0530

    Postgres support

--- src/backend/core/settings.py
+++ src/backend/core/settings.py
@@ -83,11 +83,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'caloriemeter', 
+        'USER': 'postgres', 
+        'PASSWORD': '7488579605',
+        'HOST': 'localhost', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
