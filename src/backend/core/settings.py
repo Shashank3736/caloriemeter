d4fb6ab4032e8e2fff9c6783fb82fcc04c78abc6
Shreyash Raj <shreyashr267@gmail.com>
2024-02-11 12:02:10 +0530

    Update database configuration in settings.py

--- src/backend/core/settings.py
+++ src/backend/core/settings.py
@@ -86,17 +86,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'caloriemeter',
-        'USER': 'shashank',
-        'PASSWORD': os.environ.get('PSQL_PASS'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'caloriemeter',
+            'USER': 'shashank',
+            'PASSWORD': os.environ.get('PSQL_PASS'),
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
